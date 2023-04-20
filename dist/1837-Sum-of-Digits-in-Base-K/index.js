cb95b2e0adf4bb243db73846b8886ef64168c079
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2023-04-20 00:03:59 -0400

    wip

--- dist/1837-Sum-of-Digits-in-Base-K/index.js
+++ dist/1837-Sum-of-Digits-in-Base-K/index.js
@@ -1,20 +1,20 @@
 // Input: n = 34, k = 6
 // Output: 9
 // Explanation: 34 (base 10) expressed in base 6 is 54. 5 + 4 = 9.
-// const sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
-// console.log(sum); // 6
-function sumBase(n, k) {
-    let numberArr = [];
-    const sub = n / k;
-    const rounded = Math.floor(sub);
-    numberArr = Array.from(String(rounded), Number);
-    const base = n % k;
-    numberArr.push(base);
-    const sum = numberArr.reduce((partialSum, a) => partialSum + a, 0);
-    console.log('==================');
+const sumBase = (n, k) => {
+    let sum = 0;
+    const result = [];
+    while (n > 0) {
+        const remainder = Math.floor(n / k);
+        sum += n % k;
+        if (remainder < k) {
+            sum += remainder;
+            break;
+        }
+        n = remainder;
+    }
     console.log(sum);
     return sum;
-}
-;
+};
 sumBase(68, 2);
 //# sourceMappingURL=index.js.map
--- src/1837-Sum-of-Digits-in-Base-K/index.ts
+++ src/1837-Sum-of-Digits-in-Base-K/index.ts
@@ -1,20 +1,23 @@
 // Input: n = 34, k = 6
 // Output: 9
 // Explanation: 34 (base 10) expressed in base 6 is 54. 5 + 4 = 9.
-// const sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
-// console.log(sum); // 6
 
-function sumBase(n: number, k: number): number {
-    let numberArr = [];
-    const sub:number = n/k;
-    const rounded:number = Math.floor(sub);
-    numberArr = Array.from(String(rounded), Number)
-    const base:number = n % k;
-    numberArr.push(base);
-    const sum = numberArr.reduce((partialSum, a) => partialSum + a, 0)
-    console.log('==================')
-    console.log(sum);
+const sumBase = (n: number, k: number): number => {
+    let sum = 0;
+    const result = []
+
+    while( n > 0 ){
+        const remainder = Math.floor(n/k);
+        sum += n % k;
+
+        if(remainder < k) {
+            sum += remainder;
+            break
+        }
+        n = remainder
+    }
+    console.log(sum)
     return sum
-};
+}
 
-sumBase(68, 2)
+sumBase(68,2);
